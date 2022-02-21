a78d21664bf4f35b296110048cfb505a7d7c59a3
Pascal <77117642+Pascal273@users.noreply.github.com>
2022-02-21 15:42:06 +0100

    Review (not in response to a ticket) added

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("following", views.following, name="following"),
     path("ticket/create", views.create_ticket, name="create_ticket"),
     path("review/create", views.create_review, name="create_review"),
+    path("no_ticket_review/create", views.create_ticket_with_review,
+         name="create_review_with_ticket"),
 ]
--- main/views.py
+++ main/views.py
@@ -46,3 +46,21 @@ def create_review(request):
             form.save()
             redirect('home')
     return render(request, 'main/create_review.html', {'form': form})
+
+
+@login_required
+def create_ticket_with_review(request):
+    ticket_form = forms.CreateTicketForm()
+    review_form = forms.CreateReviewForm()
+    if request.method == 'POST':
+        ticket_form = forms.CreateTicketForm(request.POST, request.FILES)
+        review_form = forms.CreateReviewForm(request.POST)
+        if all([ticket_form.is_valid(), review_form.is_valid()]):
+            ticket_form.save()
+            review_form.save()
+            return redirect('home')
+    context = {
+        'ticket_form': ticket_form,
+        'review_form': review_form,
+    }
+    return render(request, 'main/create_review_with_ticket.html', context)
