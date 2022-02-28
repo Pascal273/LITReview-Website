d2697eaeb66865c682166cf2f640c894fb089cbc
Pascal <77117642+Pascal273@users.noreply.github.com>
2022-02-28 11:34:47 +0100

    delete-Modal-window closes by clicking outside it

--- main/static/main/js/scripts.js
+++ main/static/main/js/scripts.js
@@ -1,3 +1,4 @@
+// displays the ticket-image before submitting the form
 const file = document.getElementById("id_image");
 if (file) {
   file.addEventListener("change", (event) => {
@@ -13,7 +14,6 @@ if (file) {
 
 
 // Modal window setup
-
 const delete_buttons = document.getElementsByClassName("delete-button");
 for (const delete_button of delete_buttons) {
   delete_button.addEventListener("click", () => {
@@ -22,7 +22,17 @@ for (const delete_button of delete_buttons) {
     conf_del_btn.href = delURL;
     let modalContent = document.getElementById('modal-container')
     modalContent.style.visibility = "visible"
-  })
+  });
 }
 
+
+// Modal window also closes by click outside it
+let outsideModal = document.getElementById("modal-container");
+let modalArea = document.querySelector(".modal");
+outsideModal.addEventListener("click", (event) => {
+  let isClickInside = modalArea.contains(event.target);
+  if (!isClickInside) {
+    document.getElementById('modalReturn-button').click();
+  }
+});
 
